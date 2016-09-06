b38d7b84cfe7fdf7d27765e9c37e6fe3d4bfd056
iuliust <iuliust@gmail.com>
2016-09-06 13:51:44 +0200

    commente le fichier de test provisoire

--- client/app/services/rules.spec.js
+++ client/app/services/rules.spec.js
@@ -1,17 +1,17 @@
-var mock, rulesService;
-beforeEach(module('spockApp'));
-beforeEach(function() {
-
-  inject(function($injector) {
-    rulesService = $injector.get('rulesService');
-  });
-});
-
-it('should not alert first two notifications', function() {
-  // rulesService('one');
-  // rulesService('two');
-
-  expect(rulesService.compare('rock', 'paper')).toEqual(1);
-});
-
-// pas le temps de faire de vrais tests, malheuresement.
+// var mock, rulesService;
+// beforeEach(module('spockApp'));
+// beforeEach(function() {
+//
+//   inject(function($injector) {
+//     rulesService = $injector.get('rulesService');
+//   });
+// });
+//
+// it('should not alert first two notifications', function() {
+//   // rulesService('one');
+//   // rulesService('two');
+//
+//   expect(rulesService.compare('rock', 'paper')).toBe(1);
+// });
+//
+// // pas le temps de faire de vrais tests, malheuresement.
